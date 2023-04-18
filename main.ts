5e13dd071a0e1c0234ff457f93e35e4be8f797b8
Ederson Torresini <ederson.torresini@proton.me>
2023-04-18 13:40:49 -0300

    2 LEDs que acendem no escuro

--- main.ts
+++ main.ts
@@ -1,20 +1,45 @@
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    pins.digitalWritePin(DigitalPin.P2, 1)
     basic.showIcon(IconNames.Square)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    music.playTone(440, music.beat(BeatFraction.Whole))
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    music.playTone(349, music.beat(BeatFraction.Whole))
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
     basic.showIcon(IconNames.SmallSquare)
+    music.playTone(349, music.beat(BeatFraction.Whole))
 })
-basic.showIcon(IconNames.Heart)
-loops.everyInterval(10000, function () {
-    if (input.lightLevel() <= 128) {
+loops.everyInterval(60000, function () {
+    if (input.lightLevel() <= 100) {
         pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
         basic.showIcon(IconNames.Diamond)
+    } else if (input.lightLevel() <= 200) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showIcon(IconNames.SmallDiamond)
     } else {
         pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.showIcon(IconNames.SmallDiamond)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
     }
 })
