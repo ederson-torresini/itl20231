9c7c8da991cb2b95e3763fe97c32638c0ffb36e1
Ederson Torresini <ederson.torresini@proton.me>
2023-04-24 13:21:09 -0300

    Teste com rádio: brincadeira de pega-pega

--- main.ts
+++ main.ts
@@ -1,50 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    luzremoto = receivedNumber
-    music.playTone(349, music.beat(BeatFraction.Eighth))
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    game.gameOver()
 })
-let luzlocal = 0
-let luzremoto = 0
-radio.setGroup(1)
+input.onButtonPressed(Button.A, function () {
+    canal = canal - 1
+    radio.setGroup(canal)
+    basic.showNumber(canal)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(canal)
+})
+input.onButtonPressed(Button.B, function () {
+    canal = canal + 1
+    radio.setGroup(canal)
+    basic.showNumber(canal)
+})
+let canal = 0
+canal = 1
+radio.setGroup(canal)
 radio.setTransmitPower(7)
-loops.everyInterval(1000, function () {
-    luzlocal = input.lightLevel()
-    radio.sendNumber(luzlocal)
-    basic.showLeds(`
-        # . . . .
-        . . . . .
-        # . . . .
-        . . . . .
-        # . . . .
-        `)
-    if (luzlocal <= 100 && luzremoto <= 100) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.showLeds(`
-            # . # . #
-            . . # . #
-            # . # . #
-            . . # . #
-            # . # . #
-            `)
-    } else if (luzlocal <= 200 && luzremoto <= 200) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.showLeds(`
-            # . # . .
-            . . # . .
-            # . # . #
-            . . # . #
-            # . # . #
-            `)
-    } else {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.showLeds(`
-            # . . . .
-            . . . . .
-            # . . . .
-            . . . . .
-            # . # . #
-            `)
-    }
-})
+pins.digitalWritePin(DigitalPin.P0, 0)
+basic.showNumber(canal)
