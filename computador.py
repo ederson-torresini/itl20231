0860095e9b2211d0757ff1922851cf29ae21f86f
Ederson Torresini <etorresini@ifsc.edu.br>
2023-05-07 19:29:16 -0300

    Alterar taxa de transmissão serial para 115.200bps

--- computador.py
+++ computador.py
@@ -1,7 +1,6 @@
 from sys import stdin
 from serial import Serial
 
-# Interface serial: 9.600 bps, bits 8N1
-rasp = Serial("/dev/ttyACM0")
+rasp = Serial("/dev/ttyACM0", 115200)
 for line in stdin:
     rasp.write(line.encode())
