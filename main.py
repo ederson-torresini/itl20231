66a762416e751900e5f911ba70a2416a1b292cf4
Ederson Torresini <ederson.torresini@proton.me>
2023-05-07 19:24:19 -0300

    Ler dados do computador via serial

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
-def on_received_number(receivedNumber):
-    pins.digital_write_pin(DigitalPin.P0, 1)
-    game.game_over()
-radio.on_received_number(on_received_number)
+serial.set_baud_rate(BaudRate.BAUD_RATE115200)
 
-def on_button_pressed_a():
-    global canal
-    canal = canal - 1
-    radio.set_group(canal)
-    basic.show_number(canal)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_ab():
-    radio.send_number(canal)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    global canal
-    canal = canal + 1
-    radio.set_group(canal)
-    basic.show_number(canal)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-canal = 0
-canal = 1
-radio.set_group(canal)
-radio.set_transmit_power(7)
-pins.digital_write_pin(DigitalPin.P0, 0)
-basic.show_number(canal)
+def on_forever():
+    basic.show_string(serial.read_line())
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,25 +1,4 @@
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    game.gameOver()
+serial.setBaudRate(BaudRate.BaudRate115200)
+basic.forever(function () {
+    basic.showString(serial.readLine())
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    canal = canal - 1
-    radio.setGroup(canal)
-    basic.showNumber(canal)
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    radio.sendNumber(canal)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    canal = canal + 1
-    radio.setGroup(canal)
-    basic.showNumber(canal)
-})
-let canal = 0
-canal = 1
-radio.setGroup(canal)
-radio.setTransmitPower(7)
-pins.digitalWritePin(DigitalPin.P0, 0)
-basic.showNumber(canal)
