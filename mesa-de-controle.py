313b6a352f0efdd89c43b00ed3f98384d196d127
Ederson Torresini <ederson.torresini@proton.me>
2023-06-12 17:33:00 +0000

    Adicionar quebra de linha na comunicação serial

--- mesa-de-controle.py
+++ mesa-de-controle.py
@@ -50,7 +50,7 @@ def main():
                 "".join(["itl20231/estado/", casa, "/", circuito]), payload, 1
             )
             if microbit:
-                microbit.write("".join([casa, circuito]).encode())
+                microbit.write("".join([casa, circuito, "\n"]).encode())
             else:
                 print("".join([casa, circuito]))
 
