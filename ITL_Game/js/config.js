e8772e030b7c1d499aae0c4a70d1c5362ad261c6
Ederson Torresini <ederson.torresini@proton.me>
2023-06-22 14:20:07 +0000

    Remover modo debug

--- ITL_Game/js/config.js
+++ ITL_Game/js/config.js
@@ -6,15 +6,14 @@ export default {
   physics: {
     default: "arcade",
     arcade: {
-      debug: true,
       gravity: { y: 0 },
     },
   },
   scale: {
     mode: Phaser.Scale.FIT,
-    parent: 'game-container',
+    parent: "game-container",
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 640,
-    height: 360
-  }
+    height: 360,
+  },
 };
